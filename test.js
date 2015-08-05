7467e7dd3ea5b4ee5831a194e2984e1202979454
Finn <derfinn@gmail.com>
2015-08-05 21:28:59 +0200

    add test for allowing single-column quoted csvs

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var detect = require('./')
 
 test('detect csvs from csv-spectrum (and reject jsons)', function (t) {
   spectrum(function (err, data) {
-    if(err) throw err
+    if (err) throw err
     data.forEach(function (datum) {
       t.ok(detect(datum.csv), 'csv ' + datum.name)
       t.notOk(detect(datum.json), 'json ' + datum.name)
@@ -17,13 +17,13 @@ test('detect delimiters', function (t) {
   var d
   d = detect('a,b,c\n1,2,3') || {}
   t.equals(d.delimiter, ',', ',')
-  
+
   d = detect('a;b;c\n\r1;2;3') || {}
   t.equals(d.delimiter, ';', ';')
-  
+
   d = detect('"a,,,,,,,,,"\tb\tc\n1\t2\t3') || {}
   t.equals(d.delimiter, '\t', '\\t and quotes')
-  
+
   t.end()
 })
 
@@ -38,6 +38,22 @@ test('detect newlines', function (t) {
   t.end()
 })
 
+test('one column with quotes', function (t) {
+  var d
+  d = detect('"hello"\n"test"')
+  t.equals(d.newline, '\n', 'newline')
+  t.notOk(d.delimiter) // ~false or null
+
+  d = detect('"hello ""you""\n"hi"')
+  t.equals(d.newline, '\n', 'newline')
+  t.notOk(d.delimiter) // ~false or null
+
+  d = detect('hello\nhello')
+  t.notOk(d)
+
+  d = detect('"hello""\n"hello""')
+  t.notOk(d)
+})
 
 //  should this considered be valid csv? {"a": 1, "b": 2, "c": 3}\n{"a": 1, "b": 2, "c": 3}
 // it's like ['{"a": 1', '"b": 2' ... ]
